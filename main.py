b7c14e4ee3f116e80fb2f7c3a3086d0f2c396f3e
Winnie Lam <winnieee.lam00@gmail.com>
2018-07-23 11:59:56 -0400

    use the html

---
+++ main.py
@@ -0,0 +1,11 @@
+import jinja2
+import os
+import webapp2
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
