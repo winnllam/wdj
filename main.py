efa684b523d7e5958dae81d27b9a1e0c28712d7c
Winnie Lam <winnieee.lam00@gmail.com>
2018-07-23 12:40:09 -0400

    files

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ class MainHandler(webapp2.RequestHandler):
         main_template = jinja_env.get_template('templates/main.html')
         html = main_template.render()
         self.response.write(html)
+
+class SignUpHandler(webapp2.RequestHandler):
+    def get(self):
+        sign_up_template = jinja_env.get_template('templates/signup.html')
+        html = sign_up_template.render()
+        self.response.write(html)
+
+class PersonHandler(webapp2.RequestHandler):
+    def get(self):
+        ### create models
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler), # asking for slash, construct main handlers
+    ('/signup', SignUpHandler),
+], debug = True)
