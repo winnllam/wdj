edc247225ff6ca98a20b42ed93c617801d12d8b4
Winnie Lam <winnieee.lam00@gmail.com>
2018-07-25 17:18:34 -0400

    sign up changes

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ import json
 class Person(ndb.Model):
     email = ndb.StringProperty(required = True)
     name = ndb.StringProperty(required = True)
-    college = ndb.StringProperty(required = True)
-    long1 = ndb.FloatProperty(required = True)
-    lat1 = ndb.FloatProperty(required = True)
     highschool = ndb.StringProperty(required = True)
-    long2 = ndb.FloatProperty(required = True)
-    lat2 = ndb.FloatProperty(required = True)
+    city1 = ndb.StringProperty(required = True)
+    state1 = ndb.StringProperty(required = True)
+    college = ndb.StringProperty(required = True)
+    city2 = ndb.StringProperty(required = True)
+    state2 = ndb.StringProperty(required = True)
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
             sort_keys=True, indent=4)
